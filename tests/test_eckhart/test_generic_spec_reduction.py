1d15da7a038f7f5c3b31bf1e9ecb0ecf1debfc89
Eckhart Spalding <espaldin@nd.edu>
2022-11-01 16:40:01 -0400

    removed redundant stuff

--- tests/test_eckhart/test_generic_spec_reduction.py
+++ tests/test_eckhart/test_generic_spec_reduction.py
@@ -9,7 +9,7 @@ def test_betapic(pipeline, test_dir):
 
     Simple checks of spectral extraction products 
     """
-    print('yes')
+    
     stem = "/home/prunelle/gpi2_pipeline_development/data/Reduced/140422"
     
     status, outrecipe, outfiles = pipeline.run_recipe( os.path.join(test_dir, recipename), rescanDB=True)
@@ -20,14 +20,11 @@ def test_betapic(pipeline, test_dir):
     assert len(outfiles)==nfiles_expected, "Number of output files does not match expected value."
     assert stem+"/S20140422S0338_spdc.fits" in outfiles, "Output files didn't contain one of the _spdc cubes."
     
-    """  
     # Are the contents of that file what we expected?
-    cube = gpipy.read( "./S20131118S0064_median.fits")
+    cube = gpipy.read( stem+"/S20140422S0338_spdc.fits")
     assert cube.filetype=='Spectral Cube', "Wrong output file type"
-    """
+    
     # TODO write tests here that check sat spot keyword values in headers for one of the individual files
 
     # TODO write more tests here looking at actual pixel values, to
     # verify the planet is detected as expected
-
-    assert 1<2
